db2d4e7e1478ef3f5253829406ecaf4b77a6d776
Danial Motevali <danial.motevali.82@gmail.com>
2023-10-05 11:36:51 +0330

    interface

--- App.Domain.core/Datas/EfRipository/IProductCustomAttribute.cs
+++ App.Domain.core/Datas/EfRipository/IProductCustomAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Domain.core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace App.Domain.core.Datas.EfRipository
 {
-    public interface IProductCustomAttribute
+    public interface IProductCustomAttribute : IRepository<ProductCustomAttribute>
     {
     }
 }
---
+++ App.Domain.core/Datas/EfRipository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.core.Datas.EfRipository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetByFilterAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<bool> ExistsAsync(int id);
+        Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);
+    }
+}
